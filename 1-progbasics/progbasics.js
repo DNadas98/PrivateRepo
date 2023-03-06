8a0f0aac325402d9c877ead7bd59adb404f9bada
Daniel Nadas <125133206+DNadas98@users.noreply.github.com>
2023-03-06 18:44:23 +0100

    .

---
+++ 1-progbasics/progbasics.js
@@ -0,0 +1,55 @@
+/*00. Onboarding, git:
+    codecool setup guide: https://drive.google.com/drive/folders/1o-PZujjumsUnAjZ9eP2MywMNPox7MFQs
+    NodeJS,VSCode,Git ssh
+    git ssh: https://journey.study/v2/learn/materials/tutorials/git-ssh-setup.md
+    git cheatsheet: https://journey.study/v2/learn/materials/pages/git/git-basics.md
+*/
+
+//01/01. Structuring Data
+
+
+//01/02. Logic behind coding
+
+
+//01/03. 1st Teamwork
+
+
+//02/01. Reusability with Functions
+
+
+//02/02. 2nd Teamwork
+
+
+//03/01. Builtin methods
+
+
+//03/02. Function as an argument
+
+
+//03/03. 3rd Teamwork
+
+
+//04/01. JSON
+
+
+//04/02. Structuring the View
+
+
+//04/03. 4th Teamwork
+
+
+//05/01. Events of the View
+
+
+//05/02. Design of the View
+
+
+//05/03. 5th Teamwork
+
+
+//06/01. Workbook
+
+
+//06/02. 1.PA
+
+
