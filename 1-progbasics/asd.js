12cc8e9309a6399b85c11b37aee0a15cf32d5b4d
Daniel Nadas <125133206+DNadas98@users.noreply.github.com>
2023-03-09 19:05:31 +0100

    asdfg

---
+++ 1-progbasics/asd.js
@@ -0,0 +1,12 @@
+//... operator: "unpacks" the elements of an array
+let asd1=[1,2,3,4,5,8];
+console.log(Math.max(asd1));//
+console.log(Math.max(...asd1));
+let asd2=["a","b","c","d"];
+let asd3=["e","f","g","h"];
+asd2.push(asd3);
+console.log(asd2,"\n",...asd2);
+let asd4=["a","b","c","d"];
+let asd5=["e","f","g","h"];
+asd4.push(...asd5);
+console.log(asd4,"\n",...asd4);
