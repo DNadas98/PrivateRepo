7da7c8ddc4c3e0eaaf83e982d824de217d919758
Daniel Nadas <125133206+DNadas98@users.noreply.github.com>
2023-03-12 23:31:28 +0100

    asd

--- 1-progbasics/webserver01/server.js
+++ 1-progbasics/webserver01/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();  //creates express app
