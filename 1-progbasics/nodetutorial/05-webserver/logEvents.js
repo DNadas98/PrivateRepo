79d99001fc97f420a502248f04f41fc17d4e9114
Daniel Nadas <125133206+DNadas98@users.noreply.github.com>
2023-03-13 01:16:42 +0100

    webserver 05 works, nodeJS only

--- 1-progbasics/nodetutorial/05-webserver/logEvents.js
+++ 1-progbasics/nodetutorial/05-webserver/logEvents.js
@@ -6,18 +6,19 @@ const {format}=require("date-fns");
 const{v4:uuid}=require("uuid");
 const fs=require("fs");
 const fsPromises=require("fs").promises;
+const path=require("path");
 
 //logEvents function
-const logEvents=async(message)=>{
+const logEvents=async(message,logName)=>{
   const dateTime=`${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
   const logItem=`${dateTime}\t${uuid()}\t${message}\n`;
   console.log(logItem);
   try{
-    if(!fs.existsSync("./logs")){
+    if(!fs.existsSync(path.join(__dirname,"logs"))){
       await fsPromises.mkdir("./logs");
     }
-    await fsPromises.appendFile(("./logs/eventLog.txt"),logItem);
+    await fsPromises.appendFile(path.join(__dirname,"logs",logName),logItem);
   } catch(err){
       console.log(err);
   }
--- 1-progbasics/nodetutorial/05-webserver/server.js
+++ 1-progbasics/nodetutorial/05-webserver/server.js
@@ -1,39 +1,56 @@
 //https://www.youtube.com/watch?v=3ZAKY-CDKog&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw&index=5
 //currently at https://youtu.be/3ZAKY-CDKog?t=1601
 
-
-//import common core modules
+//import common core modules http, fs, fs promises, path
 const http=require("http");
 const fs=require("fs");
 const fsPromises=require("fs").promises;
 const path=require("path");//!
-//events
-/*
-const logEvents=require("./logEvents"); //custom module
+
+//handle events https://nodejs.org/api/events.html#events_events
 const EventEmitter=require("events"); //import events
-class Emitter extends EventEmitter{}
-//initialize object
-const myEmitter=new Emitter();
-*/
-//set port
+const logEvents=require("./logEvents"); //my custom module
+class Emitter extends EventEmitter{}  
+const myEmitter=new Emitter();  //initialize object
+myEmitter.on("log",(msg,fileName)=>logEvents(msg,fileName)); //add listener for the log event
+
+//set the port
 const PORT=process.env.PORT || 3000;
+
 //
 const serveFile=async(filePath,contentType,response)=>{
   try{
-    const data=await fsPromises.readFile(filePath,"utf8");
-    response.writeHead(200,{"Content-Type":contentType});
-    response.end(data);
+    const rawData=await fsPromises.readFile(filePath,
+      contentType.includes("image")  //if it's an image, leave encoding blank, else set to utf8
+      ?""
+      :"utf8");
+    const data=contentType==="application/json" //if it's a json file, parse
+      ?JSON.parse(rawData)
+      :rawData;
+    response.writeHead(
+      filePath.includes("404.html") //don't send 200 when 404
+        ?404
+        :200,
+      {"Content-Type":contentType});
+    response.end(
+      contentType==="application/json"
+        ?JSON.stringify(data)
+        :data);
   } catch(err){
     console.log(err);
+    myEmitter.emit("log",`${err.name}: ${err.message}`,"errLog.txt"); //Emit event
     response.statusCode=500;
     response.end();
   }
 };
-//create server
+
+//create the server
 const server=http.createServer((req,res)=>{
   console.log(req.url,req.method);  //log request and request method
   //GET: browser requests site from server
   //other html methods: PUT:browser sends data to server in the url, POST: browser sends data not to the url
+  myEmitter.emit("log",`${req.url}\t${req.method}`,"reqLog.txt"); //Emit event
+
   const extension=path.extname(req.url);  //get extension name
   //define content types
   let contentType;
@@ -45,7 +62,7 @@ const server=http.createServer((req,res)=>{
       contentType="text/javascript";
       break;
     case ".json":
-      contentType="text/json";
+      contentType="application/json";
       break;
     case ".txt":
       contentType="text/plain";
@@ -57,19 +74,21 @@ const server=http.createServer((req,res)=>{
       contentType="image/png";
       break;
     default:
-      contentType="text/html";  //default: html
+      contentType="text/html";  //default: html!
   }
+
   //determine file path based on content type
   let filePath=//chained ternary statement: condition ? if_true : else_condition2 ? if_condition2_true : else_condition3 ? ...
-    contentType==="text/html"&&req.url==="/"  //condition 1(if): if its html and url is just a /
+    contentType==="text/html"&&req.url==="/"  //if its html and url is just a /
       ?path.join(__dirname,"views","index.html")  //then this will be the file shown in the browser: (./views/index.html)
-      :contentType==="text/html"&&req.url.slice(-1)==="/" //condition 2(else if): the last char is a /: (subdirectory, not just the main)
-        ?path.join(__dirname,"views",req.url,"index.html")  //then this will be the value
+      :contentType==="text/html"&&req.url.slice(-1)==="/" //(else if): the last char is a /: (subdirectory, not just the main)
+        ?path.join(__dirname,"views",req.url,"index.html")  //then same as before but include path
         :contentType==="text/html"  //condition 3: is it a html?
-          ?path.join(__dirname,"views",req.url) //look in the views folder
+          ?path.join(__dirname,"views",req.url) //if it is, look in the views folder
           :path.join(__dirname,req.url);  //look in another folder
   if(!extension&&req.url.slice(-1)!=="/") filePath+=".html";  //makes .html ext. not required in the browser
-//check to see if the file exists
+
+  //check to see if the file exists
 const fileExists=fs.existsSync(filePath);
 if(fileExists){
   //serve the file, http response
@@ -96,7 +115,3 @@ else{
 //make server listen for requests (start the server) should always be at the bottom
 server.listen(PORT,()=>console.log(`server running on port ${PORT}`));
 
-
-/*myEmitter.on("log",(msg)=>logEvents(msg)); //add listener for the log event
-//Emit event
-myEmitter.emit("log","Log event emitted!");*/
